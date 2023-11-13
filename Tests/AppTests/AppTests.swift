42c6e113de2af7e8a48d23b2316851188e4e32bc
Hollen0318 <yujianni666@icloud.com>
2023-11-13 20:35:51 +0000

    leaf page

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -12,4 +12,38 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
     }
+
+    var app: Application!
+
+    override func setUpWithError() throws {
+        app = Application(.testing)
+        try configure(app)
+    }
+
+    override func tearDownWithError() throws {
+        app.shutdown()
+    }
+
+    func testCreateComment() async throws {
+        let comment = Comment(restaurantID: 1, content: "Great food!", score: 5, time: Date())
+        try await app.test(.POST, "comments", beforeRequest: { req in
+            try req.content.encode(comment)
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertNotNil(res.body.string.contains("Great food!"))
+        })
+    }
+
+    func testGetAllComments() async throws {
+        try await app.test(.GET, "comments", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            // Check if the response contains expected data
+        })
+    }
+
+    func testDeleteComment() async throws {
+        // First, create a comment
+        // Then, test deletion of that comment
+    }
+    
 }
