074fc6fa8d0cbe7e6d5a11c70ea1ea9d4ab150e8
Hollen0318 <yujianni666@icloud.com>
2023-11-13 20:43:56 +0000

    listen to all internet

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -14,9 +14,13 @@ public func configure(_ app: Application) async throws {
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
         username: Environment.get("DATABASE_USERNAME") ?? "postgres",
         password: Environment.get("DATABASE_PASSWORD") ?? "password",
-        database: Environment.get("DATABASE_NAME") ?? "todo",
+        database: Environment.get("DATABASE_NAME") ?? "comment",
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
+
+    app.http.server.configuration.hostname = "0.0.0.0"
+    app.http.server.configuration.port = 8080 // or any other port you prefer
+
 
     app.migrations.add(CreateTodo())
     app.migrations.add(CreateComment())
